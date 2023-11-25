f88fbe6bedc08e88f4809954bf9c0bc37a337dda
Dean Freeman <dean.freemanwilde@gmail.com>
2023-11-25 13:00:08 -0500

    fundamentals-pt1: Assignment Basic Operators

--- fundamentals-pt1/assignments.js
+++ fundamentals-pt1/assignments.js
@@ -5,9 +5,17 @@ const continent = 'North America';
 const isIsland = false;
 let language = 'english';
 let population = 340.73; // In millions
-console.log(country, continent, population)
+// console.log(country, continent, population)
 
-console.log(typeof isIsland, typeof population, typeof country, typeof language)
+// console.log(typeof isIsland, typeof population, typeof country, typeof language)
 
 // ! isIsland = true
 // ! Throws TypeError: invalid assignment to const 'isIsland'
+console.log('Half of the population: ' + population / 2);
+population++;
+console.log(population);
+const populationFinland = 6;
+const populationAverage = 33;
+console.log(population > populationFinland, population > populationAverage);
+let description = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language
+console.log(description)
