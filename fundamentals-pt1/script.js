e45108b27cad6b9764266563f3713cfffcf900f9
Dean Freeman <dean.freemanwilde@gmail.com>
2023-11-25 11:24:39 -0500

    fundamentals-pt1: Script File Example

---
+++ fundamentals-pt1/script.js
@@ -0,0 +1,6 @@
+// assigns variable
+let js = 'amazing';
+// alert creates a text box containing a message
+if (js === 'amazing') alert('JavaScript is FUN!');
+// returns data to browser console
+console.log(40 + 8 + 23 - 10);
