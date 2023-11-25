1144e902b63d161480a0ed14fa182ddfed8a42ad
Dean Freeman <dean.freemanwilde@gmail.com>
2023-11-25 12:40:53 -0500

    fundamentals-pt1: Assignment let, const and var

--- fundamentals-pt1/assignments.js
+++ fundamentals-pt1/assignments.js
@@ -1,10 +1,13 @@
 // JavaScript Fundamentals – Part 1
 
-let country = 'United States of America';
-let continent = 'North America';
+const country = 'United States of America';
+const continent = 'North America';
+const isIsland = false;
+let language = 'english';
 let population = 340.73; // In millions
 console.log(country, continent, population)
 
-let isIsland = false;
-let language;
 console.log(typeof isIsland, typeof population, typeof country, typeof language)
+
+// ! isIsland = true
+// ! Throws TypeError: invalid assignment to const 'isIsland'
