0efd9e88583991932532d430be0057d94c3b7390
Dean Freeman <dean.freemanwilde@gmail.com>
2023-11-25 11:57:22 -0500

    fundamentals-pt1: Naming Style Example

--- fundamentals-pt1/script.js
+++ fundamentals-pt1/script.js
@@ -1,6 +1,28 @@
-// assigns variable
 let js = 'amazing';
-// alert creates a text box containing a message
-if (js === 'amazing') alert('JavaScript is FUN!');
-// returns data to browser console
 console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+// * Snake-case for variables is best practice
+let firstName = 'Matilda';
+// * Constants are uppercase
+let PI = 3.1415;
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+//! Technically useable, not best practice
+// let jonas_matilda = 'JM';
+// let $function = 27;
+// let name = 'Jonas';
+// let Person = 'Jonas';
+
+// * Be descriptive
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Project Manager';
+
+// ! Bad variable names
+let job1 = 'programmer';
+let job2 = 'manager';
