6540eaa3080040eebac2fafc5009c930b01f566c
Dean Freeman <dean.freemanwilde@gmail.com>
2023-11-25 12:40:49 -0500

    fundamentals-pt1: Assignment let, const and var

--- fundamentals-pt1/assignments.js
+++ fundamentals-pt1/assignments.js
@@ -3,4 +3,8 @@
 let country = 'United States of America';
 let continent = 'North America';
 let population = 340.73; // In millions
-console.log([country, continent, population])
+console.log(country, continent, population)
+
+let isIsland = false;
+let language;
+console.log(typeof isIsland, typeof population, typeof country, typeof language)
