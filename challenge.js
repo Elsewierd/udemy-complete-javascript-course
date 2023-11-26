c1cd85991e0b80647a7e112adf6f2663ab6d7211
Dean Freeman <dean.freemanwilde@gmail.com>
2023-11-26 13:16:58 -0500

    fudamentals-pt1: Challenge 2

--- challenge.js
+++ challenge.js
@@ -1,4 +1,4 @@
-// Data 1
+// Challenge 1
 const massMark = 78;
 const heightMark = 1.69;
 const massJohn = 92;
@@ -11,3 +11,11 @@ const markHigherBMI = BMIMark > BMIJohn;
 
 console.log(BMIMark, BMIJohn);
 console.log(markHigherBMI);
+
+// Challenge 2
+
+if (BMIMark > BMIJohn) {
+  console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`)
+} else {
+  console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`)
+}
