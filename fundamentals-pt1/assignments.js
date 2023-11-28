20916cfd4a213ad0d5a2e2fcac25d4091a96413a
Freeman Matthew LVN UDVO1 <FREEMAM1@ad.trw.com>
2023-11-28 11:46:01 -0500

    fundamentals-pt1 Assignment Switch Statement

--- fundamentals-pt1/assignments.js
+++ fundamentals-pt1/assignments.js
@@ -37,3 +37,26 @@ d='123' < 57;
 e=5 + 6 + '4' + 9 - 4 - 2;
 
 console.log(a,b,c,d,e);
+
+// switch statement
+
+switch(language) {
+  case 'chinese', 'mandarin':
+    console.log('MOST number of native speakers!');
+    break;
+  case 'spanish':
+    console.log('2nd place in number of native speakers');
+    break;
+  case 'english':
+    console.log('3rd place');
+    break;
+  case 'hindi':
+    console.log('Number 4');
+    break;
+  case 'arabic':
+    console.log('5th most spoken language');
+    break;
+  default:
+    console.log('Great language too :D');
+    break;
+}
